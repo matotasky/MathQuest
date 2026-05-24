0b500c0ec29397b4dded1a67bae369257e992c68
matotasky <mato.tasky@gmail.com>
2026-05-24 07:52:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  root: resolve(__dirname, '.'),
   build: {
     outDir: 'dist',
   },
